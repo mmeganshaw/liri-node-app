cb823392e870f5a75bb4b38192b32ea34865cd81
mmeganshaw <mmeganshaw@gmail.com>
2018-04-15 17:19:34 -0400

    fixed all issues with app

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
-console.log('this is loaded');
+require("dotenv/config")
 
-exports.twitter = {
+exports.Twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-exports.spotify = {
+exports.Spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -1,77 +1,98 @@
-require("dotenv").config();
 
-var keys = require('keys.js');
-var fs = require('fs');
-var request = require('request');
-var nodeArg = process.argv;
-var command = nodeArg[2];
-var title = nodeArg[3];
-var Twitter = require('twitter');
+// Fixed the problem with the dotenv file. The reason none of my code worked was because I was using the wrong command from the dotenv package.
+// I have a more recent update of the package and needed to follow instructions accordingly
+
+require("dotenv/config")
+// Requiring the Twitter NPM Package
+var Twitter = require("twitter");
+// Requiring the Spotify NPM Package
 var Spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+// Using the API Keys
+var keys = require("./keys.js");
+// Requiring the Request NPM Package
+var request = require("request");
+// Requiring FS Read/Write Package
+var fs = require('fs');
 
-var params = {
-	screen_name: 'meganshawinfo',
-	count: 20
+var client = new Twitter(keys.Twitter);
+
+var command = process.argv[2];
+
+var title = process.argv[3];
+
+
+// Twitter Logic
+
+if (command === "my-tweets") {
+    getmytweets()
 };
 
-if (command === undefined) {
-	var info = fs.readFileSync('random.txt', 'utf8')
-	var choices = info.split(',');
-	command = choices[0];
-	title = choices[1];
-}
 
-if (command === 'my-tweets') {
-	client.get('statuses/user_timeline', params, function (err, tweets, response) {
-		if (err) {
-			return console.log(err)
-		}
-		for (var i = 0; i < tweets.length; i++) {
-			console.log(tweets[i].text)
-			console.log(tweets[i].created_at)
-		}
-	})
-}
+function getmytweets () {
 
-else if (command === 'spotify-this-song') {
-	if (title === undefined) {
-		title = 'Starboy'
-	}
-	spotify.search({ type: 'track', query: title, limit: 1 }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        console.log(data.tracks.items[0].artists[0].name);
-        console.log(data.tracks.items[0].name);
-        console.log(data.tracks.items[0].album.name);
-        console.log(data.tracks.items[0].preview_url);
-    });
-}
-
-else if (command === 'movie-this'){
-    if (title === undefined){
-        title = 'Mr. Nobody'
-    }
     
+    var params = {screen_name: "meganshawinfo", count: 20};
+        
+
+        client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            if (error) {
+                console.log(error);
+            } 
+            for (var i = 0; i < tweets.length; i++) {
+                console.log(tweets[i].created_at);
+                console.log("");
+                console.log(tweets[i].text);
+            }  
+
+            
+        })
+};
+
+// Movie Logic
+
+if (command === "movie-this") {
+    findthemovie()
+};
+
+function findthemovie () {
+
+    if (title === undefined) {
+            title = "Mr. Nobody";
+            console.log("You didn't specify a movie title. Here's a good one!")
+        }
 
     request('http://www.omdbapi.com/?t=' + title + '&y=&plot=short&apikey=trilogy', function (error, response, body) {
-        var info = JSON.parse(body)
-        console.log(info.Title)
-        console.log(info.Year)
-        console.log(info.imdbRating)
-        console.log(info.Ratings[1].value)
-        console.log(info.Country)
-        console.log(info.Language)
-        console.log(info.Plot)
-        console.log(info.Actors)        
+        
+        var movie = JSON.parse(body)
+        console.log("Movie Title: ", movie.Title);
+        console.log("Year Movie Was Released: ", movie.Year);
+        console.log("Language: ", movie.Language);
+        console.log("Country: ", movie.Country);
+        console.log("Plot: ", movie.Plot);
+        console.log("Actors: ", movie.Actors);
+        console.log("imdbRating: ", movie.imdbRating);
+        console.log("Rotten Tomatoes: ", movie.Ratings[1].Value);
+    })
+};
 
-})
+
+var spotify = new Spotify(keys.Spotify);
+
+
+if (command === "spotify-this-song") {
+    spotifythissong();
 }
-else {
-    console.log("I don't know that one! Could you try one of these?")
-    console.log("my-tweets")
-    console.log("spotify-this-song")
-    console.log("movie-this")
+
+function spotifythissong () {
+
+    spotify.search({ type: 'track', query: title, limit: 1 }, function(error, response) {
+        if (error) {
+            return console.log("An error occurred! Try another request.");
+        }
+        console.log("Artist: ", response.tracks.items[0].artists[0].name);
+        console.log("Song Title: ", response.tracks.items[0].name);
+        console.log("Album Name: ", response.tracks.items[0].album.name);
+        console.log("Spotify URL: ", response.tracks.items[0].preview_url);
+    })
+
 }
